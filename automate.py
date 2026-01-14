249b68de149064709e3d330a1b2a482cbd52c39e
user <redzuan.amar15@gmail.com>
2026-01-14 15:00:25 +0800

    2.59 pm markas 4g, dari 1 fail masuk fail, masuk 1 lagi fail

--- automate.py
+++ automate.py
@@ -3,11 +3,8 @@ import shutil
 
 DOWNLOADS = "C:/Users/user1/Downloads"
 
-
 # for filename in os.listdir(DOWNLOADS):
 #     print(filename)
-
-
 
 baru = "TEST[REAL]"
 TARGET = os.path.join(DOWNLOADS, baru)
@@ -15,7 +12,7 @@ os.makedirs(TARGET, exist_ok=True)
 print(f"folder {baru} dijana!")
 print(f"Folder diletak di {TARGET}")
 
-FILE1 = (os.listdir(DOWNLOADS)[0])
+FILE1 = os.listdir(DOWNLOADS)[0]
 
 # print(test)
 source = os.path.join(DOWNLOADS,FILE1)
@@ -30,8 +27,18 @@ if os.path.isfile(source):
 else:
     print("kau merepek apa ni")
 
+# Sekarang semua file dalam baru tu kita juggle ke tepi pula hehe
+
+juggle= os.path.join(DOWNLOADS,"JUGGLE")
+os.makedirs(juggle, exist_ok=True)
+
+pegang = os.listdir(TARGET)[0]
+print(f"pegang kuat2 jangan lepas {pegang}")
+tangan = os.path.join(TARGET,pegang)
+if os.path.isfile(tangan):
+    shutil.move(tangan,juggle)
+    print(f"betul ke kat target ni?, letak si {pegang} kat juggle")
+else:
+    print("HAAA mana dia pergi?")
 
 
-
-
-# shutil.move(source, "C:/Users/user1/Downloads/TEST")
